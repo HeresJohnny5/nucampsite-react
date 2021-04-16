e39e77e16e015950feda9129c952b015059cca07
HeresJohnny5 <ericksonjohn01@hotmail.com>
2021-04-16 08:24:06 -0400

    Create new Contact Component. Add new route for /contactus.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,8 +5,8 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 
 // CUSTOM JAVASCRIPT
 import { CAMPSITES } from '../shared/campsites';
@@ -37,6 +37,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Route path="/contactus" component={Contact} /> 
           <Redirect to="/home" />
         </Switch>
         <Footer />
