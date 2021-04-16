e89f672ab2ba1989400508f8c60310fad8587d7d
HeresJohnny5 <ericksonjohn01@hotmail.com>
2021-04-15 21:51:25 -0400

    Add React Router DOM package. Add router configuration.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Home from "./HomeComponent";
 
 // CUSTOM JAVASCRIPT
 import { CAMPSITES } from '../shared/campsites';
@@ -23,11 +25,20 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
